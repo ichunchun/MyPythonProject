81dc6e3feeeaa2c134377685f82cc242d009d5cf
My Home Computer <ichunchunz@163.com>
2022-10-30 19:46:24 +0800

    添加目录

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ class mainpage:
         self.root.title("会员管理软件")
         self.root.geometry("500x300")
 
+    def creatpage(self):
+        menubar = tk.Menu(self.root)
+        menubar.add_command(label="添加会员", command="")
+        menubar.add_command(label="会员列表", command="")
+        menubar.add_command(label="删除会员", command="")
+        menubar.add_command(label="关于软件", command="")
+
 
 if __name__ == "__main__":
     # 初始化
